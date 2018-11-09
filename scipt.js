d7b68b99662216155c95a537b5f9b44e2c5c7e99
darknessinsideme <hasanu12240998@gmail.com>
2018-11-10 01:22:14 +0200

    how is it

---
+++ scipt.js
@@ -0,0 +1,20 @@
+console.log("Hello Wrold")
+let myVariable = "Hello There";
+console.log(myVariable);
+// console.log(typeof(myVariable));
+
+// document.getElementById("post").innerHTML = myVariable;
+// document.getElementById("post").innerHTML = myVariable;
+myVariable = ["Ahmad", "Jack","Marios","Victor","Ourania"]
+// document.getElementById("post").innerHTML = myVariable
+// [4+  3 ]+" "+myVariable[1];
+document.getElementById("post").innerHTML = myVariable
+[myVariable.length-1]+" "+ myVariable[1];
+let numberList = [1,2, 3, 4];
+numberList.push(6);
+numberList.pop();
+numberList.unshift("hasan");
+numberList.shift();
+myVariable(myVariable>=3)=5
+
+console.log(numberList)
